e3b1b3ff7a7f33018d927f72dccec4d63fe24008
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-07 08:43:18 +0000

    Added banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner/Banner";
 import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+      <Banner></Banner>
     </div>
   );
 }
