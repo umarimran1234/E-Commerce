719d9b3dfacdaff8c7bb566ca02051c5a0da1275
umarimran1234 <umartkd989@gmail.com>
2024-09-15 15:13:08 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`{geistSans.variable} {geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
